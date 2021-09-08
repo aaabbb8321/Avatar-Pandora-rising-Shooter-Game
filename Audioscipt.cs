52875feafb28e51ea7019887212272413006a14d
Meet Kachhadiya <69210584+aaabbb8321@users.noreply.github.com>
2021-09-09 00:48:19 +0530

    Add files via upload

---
+++ Audioscipt.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Audioscipt : MonoBehaviour
+{
+    [SerializeField]
+    private AudioSource pp;
+    [SerializeField]
+    private AudioSource vv;
+    [SerializeField]
+    private AudioSource cc;
+    [SerializeField]
+    private AudioSource dd;
+    // Start is called before the first frame update
+    void Start()
+    {
+        pp.PlayDelayed(3f);
+        vv.PlayDelayed(15f);
+        cc.PlayDelayed(30f);
+        dd.PlayDelayed(45f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
